f61b270e7232096159498738c4a2225b8a123788
ismail <yako_is@yahoo.fr>
2023-09-28 23:53:43 +0100

    Ca

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,15 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+{/* write an <About> component here */ }
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
